7cb4216d31257d918c9f048ec0b9adbb072f3e1b
CyTon Code <cytoncode@gmail.com>
2021-04-15 23:40:28 +0300

    system commit

---
+++ cmake-build-debug/CMakeFiles/CyTon_attempt_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CyTon_attempt_2.dir/main.cpp.o"
+  "CyTon_attempt_2"
+  "CyTon_attempt_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CyTon_attempt_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
